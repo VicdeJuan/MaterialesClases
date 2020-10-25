125f4bb685eced1acb5d482749487b3457d5d533
Víctor de Juan <dejuan.victor@gmail.com>
2020-10-25 20:08:42 +0100

    Inferencia estadística CCSS2

--- binom.py
+++ binom.py
@@ -1,6 +1,8 @@
 #!/usr/bin/ython
 
 import sys
+from decimal import *
+
 
 if len(sys.argv) != 5:
     print "Usage: "+sys.argv[0]+" n p k op\n"
@@ -13,18 +15,20 @@ if len(sys.argv) != 5:
 n=int(sys.argv[1])
 _arr_p = sys.argv[2].split("/")
 if len (_arr_p) == 1:
-    p = float(_arr_p[0])
+    p = Decimal(_arr_p[0])
 elif len(_arr_p) == 2:
-    p = float(_arr_p[0])*1.0/float(_arr_p[1])
+    p = Decimal(_arr_p[0])*1.0/Decimal(_arr_p[1])
 k=int(sys.argv[3])
 
 op=sys.argv[4]
 
 def fact(n):
-    if n<1:
-        return 1
-    else:
-        return n*fact(n-1)
+	m=Decimal(1)
+	for i in range(1,n+1):
+		m = m*i
+	return m
+
+
 
 def comb(m,n):
     return fact(n)/(fact(n-m)*fact(m))
@@ -32,9 +36,7 @@ def comb(m,n):
 def binom(n,p,k):
     return comb(k,n)*(p**k)*((1-p)**(n-k))
 
-n=30
-p=1.0/3
-a=0
+a=Decimal(0)
 
 if op == "<":
     for i in range(k):
@@ -45,7 +47,15 @@ elif op == "<=":
 elif op == "=":
     a=binom(n,p,k)
 
+elif op == ">":
+	for i in range(k+1):
+		a+=binom(n,p,i)
+	a = 1-a
+elif op == ">=":
+	for i in range(k):
+		a+=binom(n,p,i)
+	a = 1-a
 else:
-    print "not yet"
+	print "not yet"
 
 print a
